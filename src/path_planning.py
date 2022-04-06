e671d6f318498cd784659f1515680bab6b362f1c
ama29 <ma.aileen29@gmail.com>
2022-04-06 19:22:48 +0000

    Aileen working on A* in progress

--- src/path_planning.py
+++ src/path_planning.py
@@ -13,6 +13,10 @@ class PathPlan(object):
     current car pose.
     """
     def __init__(self):
+        self.map_acquired = False
+        self.start = None
+        self.end = None
+
         self.odom_topic = rospy.get_param("~odom_topic")
         self.map_sub = rospy.Subscriber("/map", OccupancyGrid, self.map_cb)
         self.trajectory = LineTrajectory("/planned_trajectory")
@@ -22,24 +26,129 @@ class PathPlan(object):
 
 
     def map_cb(self, msg):
-        pass ## REMOVE AND FILL IN ##
+        self.obstacles = []
+        data = msg.data
+        grid = data.reshape((msg.info.height, msg.info.width))
+        # for u in range(len(grid)):
+        #     for v in range(len(grid[u])):
+        #         if grid[v][u] > 50: # this number is randomly chosen
 
+
+            
+        self.map_acquired = True
+        pass ## REMOVE AND FILL IN ##
+        
 
     def odom_cb(self, msg):
-        pass ## REMOVE AND FILL IN ##
-
+        # pass ## REMOVE AND FILL IN ##
+        x = msg.twist.twist.linear.x
+        y = msg.twist.twist.linear.y
+        theta = msg.twist.twist.angular.z
+        self.start = [x,y,theta]
 
     def goal_cb(self, msg):
         pass ## REMOVE AND FILL IN ##
 
     def plan_path(self, start_point, end_point, map):
         ## CODE FOR PATH PLANNING ##
+        # Create start and end node
+        start_node = Node(None, start_point)
+        start_point.g = start_node.h = start_node.f = 0
+        end_node = Node(None, end_point)
+        end_node.g = end_node.h = end_node.f = 0
+
+        # Initialize both open and closed list
+        open_list = []
+        closed_list = []
+
+        # Add the start node
+        open_list.append(start_node)
+
+        # Loop until you find the end
+        while len(open_list) > 0:
+
+            # Get the current node
+            current_node = open_list[0]
+            current_index = 0
+            for index, item in enumerate(open_list):
+                if item.f < current_node.f:
+                    current_node = item
+                    current_index = index
+
+            # Pop current off open list, add to closed list
+            open_list.pop(current_index)
+            closed_list.append(current_node)
+
+            # Found the goal
+            if current_node == end_node:
+                path = []
+                current = current_node
+                while current is not None:
+                    path.append(current.position)
+                    current = current.parent
+                return path[::-1] # Return reversed path
+
+            # Generate children
+            children = []
+            for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+                # Get node position
+                node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+                # Make sure within range
+                if node_position[0] > (len(map) - 1) or node_position[0] < 0 or node_position[1] > (len(map[len(map)-1]) -1) or node_position[1] < 0:
+                    continue
+
+                # Make sure walkable terrain
+                if map[node_position[0]][node_position[1]] != 0:
+                    continue
+
+                # Create new node
+                new_node = Node(current_node, node_position)
+
+                # Append
+                children.append(new_node)
+
+            # Loop through children
+            for child in children:
+
+                # Child is on the closed list
+                for closed_child in closed_list:
+                    if child == closed_child:
+                        continue
+
+                # Create the f, g, and h values
+                child.g = current_node.g + 1
+                child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+                child.f = child.g + child.h
+
+                # Child is already in the open list
+                for open_node in open_list:
+                    if child == open_node and child.g > open_node.g:
+                        continue
+
+                # Add the child to the open list
+                open_list.append(child)
 
         # publish trajectory
         self.traj_pub.publish(self.trajectory.toPoseArray())
 
         # visualize trajectory Markers
         self.trajectory.publish_viz()
+
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
 
 
 if __name__=="__main__":
