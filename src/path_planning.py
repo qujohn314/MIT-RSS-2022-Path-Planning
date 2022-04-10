389eed1fe79bddd9481f970805445965d8b39f28
ama29 <ma.aileen29@gmail.com>
2022-04-10 23:29:27 +0000

    implementing priority queue to a*

--- src/path_planning.py
+++ src/path_planning.py
@@ -2,11 +2,13 @@
 
 import rospy
 import numpy as np
+import math
 from geometry_msgs.msg import PoseStamped, PoseArray
 from nav_msgs.msg import Odometry, OccupancyGrid
 import rospkg
 import time, os
 from utils import LineTrajectory
+from queue import PriorityQueue
 
 class PathPlan(object):
     """ Listens for goal pose published by RViz and uses it to plan a path from
@@ -16,6 +18,7 @@ class PathPlan(object):
         self.map_acquired = False
         self.start = None
         self.end = None
+        self.obstacles = []
 
         self.odom_topic = rospy.get_param("~odom_topic")
         self.map_sub = rospy.Subscriber("/map", OccupancyGrid, self.map_cb)
@@ -26,21 +29,49 @@ class PathPlan(object):
 
 
     def map_cb(self, msg):
-        self.obstacles = []
         data = msg.data
         grid = data.reshape((msg.info.height, msg.info.width))
-        # for u in range(len(grid)):
-        #     for v in range(len(grid[u])):
-        #         if grid[v][u] > 50: # this number is randomly chosen
-
-
-            
+        for x in range(len(grid)):
+            for y in range(len(grid[x])):
+                if grid[y][x] != 0: # this number is randomly chosen
+                    self.obstacles.append(self.convert_xy_to_uv(msg, x, y))
         self.map_acquired = True
         pass ## REMOVE AND FILL IN ##
+
+    def convert_xy_to_uv(self, msg, x, y):
+        scaled_x = float(x)/msg.info.resolution
+        scaled_y = float(y)/msg.info.resolution
+        coordinates = np.array([scaled_x, scaled_y, 1])
+        Q = msg.info.origin.orientation
+        q0 = Q.x
+        q1 = Q.y
+        q2 = Q.z
+        q3 = Q.w
+        r00 = 2 * (q0*q0 + q1*q1) - 1
+        r01 = 2 * (q1*q2 - q0*q3)
+        r02 = 2 * (q1*q3 + q0*q2)
+        # Second row of the rotation matrix
+        r10 = 2 * (q1*q2 + q0*q3)
+        r11 = 2 * (q0*q0 + q2*q2) - 1
+        r12 = 2 * (q2*q3 - q0*q1)
+        # Third row of the rotation matrix
+        r20 = 2 * (q1*q3 - q0*q2)
+        r21 = 2 * (q2*q3 + q0*q1)
+        r22 = 2 * (q0*q0 + q3*q3) - 1
         
+        # 3x3 rotation matrix
+        rot_matrix = np.array([[r00, r01, r02],
+                            [r10, r11, r12],
+                            [r20, r21, r22]])
+                            
+        rotated = np.matmul(rot_matrix, coordinates.T)
+        translation = np.array([msg.info.origin.position.x, msg.info.origin.position.y, msg.info.origin.position.z])
+        return rotated + translation.T
 
     def odom_cb(self, msg):
         # pass ## REMOVE AND FILL IN ##
+        if not self.map_acquired:
+            return
         x = msg.twist.twist.linear.x
         y = msg.twist.twist.linear.y
         theta = msg.twist.twist.angular.z
@@ -129,6 +160,23 @@ class PathPlan(object):
 
                 # Add the child to the open list
                 open_list.append(child)
+
+        # convert the path to a trajectory
+        trajectory = [] #initialize series of piecewise points
+        for i in range(len(path)-1):
+            current_node = path[i]
+            next_node = path[i+1]
+
+            delta_x = next_node.position[0] - current_node.position[0]
+            delta_y = next_node.position[1] - current_node.position[1]
+            delta_theta = delta_theta_r = math.tan(delta_y/delta_x)
+
+            #convert theta to robot coordinate frame
+            rotation_matrix = np.array()
+            delta_W = np.dot(np.array(delta_x, delta_y, delta_theta), rotation_matrix)
+            
+            trajectory.append(delta_W)
+
 
         # publish trajectory
         self.traj_pub.publish(self.trajectory.toPoseArray())
