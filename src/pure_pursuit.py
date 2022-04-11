43e3960d2c2cd3d9a7c82d5474a51a0442e44b67
SpacePanda-12 <dfgoff22@gmail.com>
2022-04-11 19:11:51 +0000

    more bug fixes

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -16,23 +16,27 @@ class PurePursuit(object):
     """ Implements Pure Pursuit trajectory tracking with a fixed lookahead and speed.
     """
     def __init__(self):
-        self.odom_topic = rospy.get_param("~odom_topic")
+        # self.odom_topic = rospy.get_param("~odom_topic")  # TODO: This is throwing an error for some reason
+        self.odom_topic = "/odom"
         self.lookahead = 1  # TODO: Refine this number
         self.speed = 1  # TODO: Any changes needed? Do we need to get speed as parameter?
         # self.wrap  = # TODO: what is this?
         self.wheelbase_length = 0.55
-        self.trajectory  = utils.LineTrajectory("/followed_trajectory")
+        self.trajectory = utils.LineTrajectory("/followed_trajectory")
         self.traj_sub = rospy.Subscriber("/trajectory/current", PoseArray, self.trajectory_callback, queue_size=1)
         self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
         self.drive_cmd = AckermannDriveStamped()
         self.drive_cmd.drive.speed = self.speed  # TODO: do we need to subscribe to this? Or can we pick speed?
         # TODO Assuming we pick for now
         self.odom_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odom_callback, queue_size=1)
+        self.trajectory_points = np.array([0])
 
     def trajectory_callback(self, msg):
         """
         Clears the currently followed trajectory, and loads the new one from the message
         """
+        if np.size(self.trajectory_points < 3):
+            pass
         # print "Receiving new trajectory:", len(msg.poses), "points"
         self.trajectory.clear()
         self.trajectory.fromPoseArray(msg)
@@ -42,7 +46,8 @@ class PurePursuit(object):
     def odom_callback(self, msg):  # assuming we get result of localization
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
-        orientation = tf.transformations.euler_from_quaternion(msg.pose.orientation)
+        quat = msg.pose.pose.orientation
+        orientation = tf.transformations.euler_from_quaternion(np.array([quat.x, quat.y, quat.z, quat.w]))
         theta = orientation[2]
         self.car_point = (x, y)
         self.pure_pursuit(theta)
@@ -66,16 +71,18 @@ class PurePursuit(object):
         return point
 
     def pure_pursuit(self, theta): # not sure what the arguments should be yet
+        if np.size(self.trajectory_points) < 3:
+            pass
         car_x = self.car_point[0]
         car_y = self.car_point[1]
         dist = lambda p: np.sqrt((car_x - p[0])**2 + (car_y - p[1])**2)
         angle = lambda p: np.arctan2(p[0], p[1])
 
         # find the differences between the x and y values of current location and points
-        points_diff = np.apply_along_axis(self.diff, self.trajectory_points)
+        points_diff = np.apply_along_axis(self.diff, self.trajectory_points, 0)
         
         # find relative angle of car to all points
-        angles = np.apply_along_axis(angle, points_diff)
+        angles = np.apply_along_axis(angle, points_diff, 0)
         
         # pick out points that are in front of us (-90 < angle < 90 degrees)
         front_trajectory_points = np.any(abs(angles) < np.pi/2)
@@ -83,7 +90,7 @@ class PurePursuit(object):
         front_points = self.trajectory_points[angles_idx]
 
         # find closest point in front of the car
-        point_distances = np.apply_along_axis(dist, front_points, 1)
+        point_distances = np.apply_along_axis(dist, front_points, 0)
         closest_point = np.minimum(point_distances)
         idx = np.where(point_distances == closest_point)
         (point_x, point_y) = self.trajectory_points[idx]
